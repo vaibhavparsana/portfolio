dcf4193d5a01d29cb6dd432317df53de63c034ba
vaibhavparsana <parsana7v@gmail.com>
2021-02-14 18:40:39 -0500

    'changes'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import udemyCCP from './images/c-aws.png';
-import kanbanPractitioner from './images/c-tkp.png';
-import frontEnd from './images/c-front-end.png';
-import javascriptAlgo from './images/c-javascript.png';
-import webDesign from './images/c-web.png';
+import udemyCCP from './images/c-aws.jpg';
+import kanbanPractitioner from './images/c-tkp.jpg';
+import frontEnd from './images/c-front-end.jpg';
+import javascriptAlgo from './images/c-javascript.jpg';
+import webDesign from './images/c-web.jpg';
 import astitva from './images/p-astistva.jpg';
-import drumMachine from './images/p-drum-machine.png';
-import jsCalc from './images/p-calculator.png';
-import pomodoro from './images/p-pomodoro-clock.png';
-import rqg from './images/p-quote.png';
+import drumMachine from './images/p-drum-machine.jpg';
+import jsCalc from './images/p-calculator.jpg';
+import pomodoro from './images/p-pomodoro-clock.jpg';
+import rqg from './images/p-quote.jpg';
 import avsar from './images/p-avsar.jpg';
 import rock from './images/p-rock.jpg';
 import table from './images/p-table.jpg';
@@ -15,7 +15,7 @@ import value from './images/p-value.jpg';
 import pform from './images/p-form.jpg';
 import chart from './images/p-chart-2.jpg';
 import bar_chart from './images/p-bar-chart.jpg';
-import wlBg from './images/welcome-background.PNG';
+// import wlBg from './images/welcome-background.jpg';
 // import skillsImage from './images/Skill-image.jpg';
 import './App.css';
 
@@ -58,7 +58,7 @@ function App() {
           <div className=" row justify-content-around">
             <div className="col-md-4">
               <h4 className='section-title'><i className="fa fa-user icon-color mr-3"></i>About Me</h4>
-              <p className="card-text">I am a self-motivated individual with about two years of learning experience at a coding Bootcamp and six years experience in electronics and communications seeking a challenging role as front end Developer in an organization that values my qualifications and my ability to deliver quality products.</p>
+              <p className="card-text">I am a self-motivated individual with about two years of learning experience at a coding Bootcamp seeking a challenging role as Developer in an organization that values my qualifications and my ability to deliver quality products.</p>
             </div>
             <div className="col-md-4">
               <h4 className='section-title'><i className="fa fa-pencil icon-color mr-3"></i>Knowledge</h4>
@@ -90,7 +90,7 @@ function App() {
       <div id='skills'  className=''>
 
         <div className="container">
-          <SectionHeader section_title='My Skills' icon_name="fa fa-tool" />
+          <SectionHeader section_title='My Skills' icon_name='fa fa-desktop' />
           <div className='row'>
             <div className='col-md-1'></div>
             <div className='col-md-4'>
@@ -123,7 +123,7 @@ function App() {
             <AchievementContent achievementImage={kanbanPractitioner} achievementImageAlt='Kanban_Practitioner' achievementTitle='Team Kanban Practitioner' achievementDesc='Learned how to organize your work, improve transparency, identify bottlenecks, visualize workflow, improve communication, and enhance team collaboration' achievementCerti='https://edu.kanban.university/users/vaibhav-parsana'/>
          
             <AchievementContent achievementImage={webDesign} achievementImageAlt='Responsive_Web_Design' achievementTitle='Responsive Web Design' achievementDesc='HTML, HTML5, Basic CSS, CSS Flexbox, CSS Grid, Applied Visual Design, Responsive Web Design Principles, Applied Accessibility' achievementCerti="https://freecodecamp.org/certification/vaibhavparsana/responsive-web-design" />
-            <AchievementContent achievementImage={javascriptAlgo} achievementImageAlt='JavaScript_Algorithms' achievementTitle='JavaScript Algorithms and Data Structure' achievementDesc='JavaScript, ES6, Regular Expressions, Data Structures, Algorithm, Object-Oriented Programming, Functional Programming, Debugging' achievementCerti='https://www.freecodecamp.org/certification/vaibhavparsana/javascript-algorithms-and-data-structures'/>
+            <AchievementContent achievementImage={javascriptAlgo} achievementImageAlt='JavaScript_Algorithms' achievementTitle='JavaScript Algorithms & Data Structure' achievementDesc='JavaScript, ES6, Regular Expressions, Data Structures, Algorithm, Object-Oriented Programming, Functional Programming, Debugging' achievementCerti='https://www.freecodecamp.org/certification/vaibhavparsana/javascript-algorithms-and-data-structures'/>
          
             <AchievementContent achievementImage={frontEnd} achievementImageAlt='Front_End_Libraries' achievementTitle='Front End Libraries' achievementDesc='Bootstrap, Jquery, Sass, React, Redux, React and Redux' achievementCerti='https://www.freecodecamp.org/certification/vaibhavparsana/front-end-libraries'/>
           </div>
@@ -135,28 +135,28 @@ function App() {
         <SectionHeader section_title='My Portfolio' icon_name="fa fa-briefcase"/>
         <div className="container ">
           <div className='row justify-content-around'>
-            <PortfolioContent portfolioImage={rqg} portfolioImageAlt='Random_quote_genereator' portfolioTitle='Random Quote Generator' portfolioDesc='A React Application to generate random quotes. It is a one of the project from the front end libraries certification of freecodecamp.' portfolioLink='https://master.d8c9xlg665lm7.amplifyapp.com/' />
-            <PortfolioContent portfolioImage={jsCalc} portfolioImageAlt='Javascript-calculator' portfolioTitle='Calculator' portfolioDesc='This is a react application for calculator. It is a one of the project from the front end libraries certification of freecodecamp.' portfolioLink='https://master.dflli073n6woh.amplifyapp.com/' />
-            <PortfolioContent portfolioImage={pomodoro} portfolioImageAlt='Pomodoro-clock' portfolioTitle='Pomodoro Clock' portfolioDesc='A Pomodoro Clock is a time management clock. This is a react application for pomodoro clock. It is a one of the project from the front end libraries certification of freecodecamp.' portfolioLink='https://master.d24nfqk1xjrn42.amplifyapp.com/' />
+            <PortfolioContent portfolioImage={rqg} portfolioImageAlt='Random_quote_genereator' portfolioTitle='Random Quote Generator' portfolioDesc='A React Application to generate random quotes.' category='Freecodecamp Project' portfolioLink='https://master.d8c9xlg665lm7.amplifyapp.com/' />
+            <PortfolioContent portfolioImage={jsCalc} portfolioImageAlt='Javascript-calculator' portfolioTitle='Calculator' portfolioDesc='This is a react application for calculator.'  category='Freecodecamp Project' portfolioLink='https://master.dflli073n6woh.amplifyapp.com/' />
+            <PortfolioContent portfolioImage={pomodoro} portfolioImageAlt='Pomodoro-clock' portfolioTitle='Pomodoro Clock' portfolioDesc='A Pomodoro Clock is a time management clock. This is a react application for pomodoro clock.' category='Freecodecamp Project' portfolioLink='https://master.d24nfqk1xjrn42.amplifyapp.com/' />
          
-            <PortfolioContent portfolioImage={astitva} portfolioImageAlt='Astitva' portfolioTitle='Astitva' portfolioDesc='The Astitva is a organization who works to help children. This is a Static HTML/Javascript Website.' portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/astitva/' />
-            <PortfolioContent portfolioImage={avsar} portfolioImageAlt='Avsar' portfolioTitle='Avsar' portfolioDesc='The Avsar is a website for purchase different watches/clocks. This is a Static HTML/Javascript Website.' portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/avsar/' />
-            <PortfolioContent portfolioImage={drumMachine} portfolioImageAlt='Drum-machine' portfolioTitle='Drum machine' portfolioDesc='This is a React Application fotr drum machine. It has different keys which will be triggered by user in order to play music. It is a one of the project from the front end libraries certification of freecodecamp.' portfolioLink='https://master.ddai3eze139cs.amplifyapp.com/' />
+            <PortfolioContent portfolioImage={astitva} portfolioImageAlt='Astitva' portfolioTitle='Astitva' portfolioDesc='The Astitva is a organization who works to help children. This is a Static HTML/Javascript Website.' category='Personal Project' portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/astitva/' />
+            <PortfolioContent portfolioImage={avsar} portfolioImageAlt='Avsar' portfolioTitle='Avsar' portfolioDesc='The Avsar is a website for purchase different watches/clocks. This is a Static HTML/Javascript Website.' category='Personal Project' portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/avsar/' />
+            <PortfolioContent portfolioImage={drumMachine} portfolioImageAlt='Drum-machine' portfolioTitle='Drum machine' portfolioDesc='This is a React Application fotr drum machine. It has different keys which will be triggered by user in order to play music.'  category='Freecodecamp Project' portfolioLink='https://master.ddai3eze139cs.amplifyapp.com/' />
          
-            <PortfolioContent portfolioImage={rock} portfolioImageAlt='rock-paper-scissor' portfolioTitle='Rock Paper Scissor Game' portfolioDesc='This is a Static HTML/Javascript Website to play game rock paper and scissor with computer. It taks value ffrom users and display result using prompt box.' portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/rock-paper-scissor/' />
-            <PortfolioContent portfolioImage={value} portfolioImageAlt='value-conversion' portfolioTitle='Value Conversion' portfolioDesc='This a Static HTML/Javascript Website to convert value into different measurement entities for example celsius, fahrenhites, feet, meters, inches, centimeters, pounds and kilograms.' portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/value-conversion/' />
-            <PortfolioContent portfolioImage={table} portfolioImageAlt='json-data-display' portfolioTitle='Json Data Display' portfolioDesc='This is a Static HTML/Javascript Webpage to display data from the json file into to table form using jQuery.' portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/json-data-display/' />
+            <PortfolioContent portfolioImage={rock} portfolioImageAlt='rock-paper-scissor' portfolioTitle='Rock Paper Scissor Game' portfolioDesc='This is a Static HTML/Javascript Website to play game rock paper and scissor with computer. It takes value from users and display result using prompt box.'  category='Acadamic Project' portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/rock-paper-scissor/' />
+            <PortfolioContent portfolioImage={value} portfolioImageAlt='value-conversion' portfolioTitle='Value Conversion' portfolioDesc='This a Static HTML/Javascript Website to convert value into different measurement entities for example celsius, fahrenhites, feet, meters, inches, centimeters, pounds and kilograms.'   category='Acadamic Project' portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/value-conversion/' />
+            <PortfolioContent portfolioImage={table} portfolioImageAlt='json-data-display' portfolioTitle='Json Data Display' portfolioDesc='This is a Static HTML/Javascript Webpage to display data from the json file into to table form using jQuery.'   category='Acadamic Project' portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/json-data-display/' />
        
-            <PortfolioContent portfolioImage={pform} portfolioImageAlt='Rental-application' portfolioTitle='Rental Application' portfolioDesc='This is a Static HTML/Javascript Webpage for rental application form.' portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/Rental-application/' />
-            <PortfolioContent portfolioImage={bar_chart} portfolioImageAlt='Bar Chart' portfolioTitle='Bar Chart' portfolioDesc='This is a Static HTML/Javascript Webpage to generate bar chart using Data Visualication(D3). It is a one of the project from the data visualization certification of freecodecamp.' portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/bar-chart/' />
-            <PortfolioContent portfolioImage={chart} portfolioImageAlt='Scatterplot Graph' portfolioTitle='Scatterplot Graph' portfolioDesc='This is a Static HTML/Javascript Webpage to generate Scatterplot Graph using Data Visualication(D3). It is a one of the project from the data visualization certification of freecodecamp.' portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/scatterplot-graph/' />
+            <PortfolioContent portfolioImage={pform} portfolioImageAlt='Rental-application' portfolioTitle='Rental Application' portfolioDesc='This is a Static HTML/Javascript Webpage for rental application form.'  category='Acadamic Project'  portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/Rental-application/' />
+            <PortfolioContent portfolioImage={bar_chart} portfolioImageAlt='Bar Chart' portfolioTitle='Bar Chart' portfolioDesc='This is a Static HTML/Javascript Webpage to generate bar chart using Data Visualication(D3).' category='Freecodecamp Project' portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/bar-chart/' />
+            <PortfolioContent portfolioImage={chart} portfolioImageAlt='Scatterplot Graph' portfolioTitle='Scatterplot Graph' portfolioDesc='This is a Static HTML/Javascript Webpage to generate Scatterplot Graph using Data Visualication(D3).' category='Freecodecamp Project' portfolioLink='http://vaibhav-parsana-portfolio-sites.s3-website-us-east-1.amazonaws.com/scatterplot-graph/' />
           </div>
         </div>
         <Divider space="70" />
       </div>
 
       <div id='experience'  className=''>
-        <SectionHeader section_title='My Experience' icon_name='fa fa-user-tie' />
+        <SectionHeader section_title='My Experience' icon_name='fa fa-building-o' />
         <div className="container">
           <div className="row experience-content">
             <div className="left-content col-md-3">
@@ -183,7 +183,7 @@ function App() {
       </div>
 
       <div id='education'  className=''>
-        <SectionHeader section_title='My Education' icon_name="fa fa-user-graduate" />
+        <SectionHeader section_title='My Education' icon_name="fa fa-graduation-cap" />
         <div className="container">
           <EducationContent certificate="Ontario College Diploma" majors="Computer Systems Technician – Software Engineering" collegename="Sheridan College" year="June 2019 - Aug 2020" />
           <EducationContent certificate="Bachelor's Engineering" majors="Electronics and Telecommunication Engineering" collegename="BVM Collge of Engineering" year="June 2014 - May 2017" />
@@ -208,7 +208,7 @@ function ProgressBar(props) {
 
 function SectionHeader(props) {
   return <div className='container'><div className="row"><div className="col-md-12">
-    <h4 className='section-title text-center'>{props.section_title}</h4>
+    <h4 className='section-title text-center'><i className={"icon-color mr-3 "+ props.icon_name}></i>{props.section_title}</h4>
   </div>
   </div>
   </div>
@@ -233,13 +233,15 @@ function AchievementContent(props) {
 }
 
 function PortfolioContent(props) {
-  return <div className="col-md-4 my-2">
+  return <div className="col-md-4 my-3">
     <div className="card portfolio-card">
     <img src={props.portfolioImage} className="card-img-top" alt={props.portfolioImageAlt} />
     <div className="card-body bg-portfolio">
       <h5 className="card-title">{props.portfolioTitle}</h5>
       <p className="card-text">{props.portfolioDesc}</p>
-      <a href={props.portfolioLink} target="_blank" className="btn btn-primary">View</a>
+     
+      <a href={props.portfolioLink} target="_blank" className="btn btn-primary">View</a><br />
+      <span class="badge badge-pill badge-secondary mt-4">{props.category}</span>
     </div>
     </div>
   </div>
