a1e131021ff93345308ba0dfd991658322c61e0e
vaibhavparsana <parsana7v@gmail.com>
2021-03-23 15:41:57 -0400

    git init

--- src/App.js
+++ src/App.js
@@ -137,7 +137,7 @@ function App() {
         <SectionHeader section_title='My Portfolio' icon_name="fa fa-briefcase" />
         <div className="container ">
           <div className='row justify-content-around'>
-            <PortfolioContent portfolioImage={rqg} portfolioImageAlt='csservices' portfolioTitle='Carrier Safety Services' portfolioDesc='A website which provides a trucking safety services.' category='Client Project' portfolioLink='https://https://carriersafetyservices.com/' />
+            <PortfolioContent portfolioImage={csservices} portfolioImageAlt='csservices' portfolioTitle='Carrier Safety Services' portfolioDesc='A website which provides a trucking safety services.' category='Client Project' portfolioLink='https://https://carriersafetyservices.com/' />
             <PortfolioContent portfolioImage={rqg} portfolioImageAlt='Random_quote_genereator' portfolioTitle='Random Quote Generator' portfolioDesc='A React Application to generate random quotes.' category='Freecodecamp Project' portfolioLink='https://master.d8c9xlg665lm7.amplifyapp.com/' />
             <PortfolioContent portfolioImage={jsCalc} portfolioImageAlt='Javascript-calculator' portfolioTitle='Calculator' portfolioDesc='This is a react application for calculator.' category='Freecodecamp Project' portfolioLink='https://master.dflli073n6woh.amplifyapp.com/' />
             <PortfolioContent portfolioImage={pomodoro} portfolioImageAlt='Pomodoro-clock' portfolioTitle='Pomodoro Clock' portfolioDesc='A Pomodoro Clock is a time management clock. This is a react application for pomodoro clock.' category='Freecodecamp Project' portfolioLink='https://master.d24nfqk1xjrn42.amplifyapp.com/' />
